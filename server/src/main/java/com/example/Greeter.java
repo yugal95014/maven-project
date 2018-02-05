b82918f648c1b52b6aa960f0e378a472ad99ae2f
yugal95014 <31354645+yugal95014@users.noreply.github.com>
2018-02-05 12:34:25 -0800

    fix Checkstyle warning

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -9,13 +9,12 @@ public class Greeter {
    * This is a constructor.
    */
   public Greeter() {
-
   }
-  
  /** 
    * @param someone greeing
    * @return greeting string
    */
+  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
